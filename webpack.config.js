5941fb199d36dea81fcd002e97c276c7e9f26c6e
zakirov <zakirov@dev.com>
2019-01-13 16:18:50 +0300

    init project

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require('path');
+
+module.exports = {
+    plugins: [
+        new MiniCssExtractPlugin({
+            // Options similar to the same options in webpackOptions.output
+            // both options are optional
+            filename: "[name].css",
+            chunkFilename: "[id].css"
+        })
+    ],
+    entry: './handwork/handicrafts/static/js/main.js',
+    output: {
+        path: path.resolve(__dirname, './handwork/handicrafts/static/dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [{
+            test: /\.css$/,
+            use: [{
+                    loader: MiniCssExtractPlugin.loader,
+                    options: {
+                        // you can specify a publicPath here
+                        // by default it use publicPath in webpackOptions.output
+                        publicPath: '../'
+                    }
+                },
+                "css-loader"
+            ]
+        }],
+    }
+};
